be704f97828385ee5e4173e71c69b20a2b8a5863
William van der Kooij <wkooij@REAGRONINGEN.ICT>
2021-05-25 13:57:45 +0200

    rewrite

--- TicketMachine.py
+++ TicketMachine.py
@@ -1,56 +1,56 @@
 #01-04-2021 this program is there to allow visitors to buy tickets,
 #get a receipt and get tickets. As well as entering relevant data into a
 #database so the park gates can check the tickets.
+#25-05-2021 Restart project after learning more wxpython.
 
 #Importing Requirements
 import wx #WX Python, GUI
 
-class MainWindow(wx.Frame):
+class Moria(wx.Frame):
     """
     Main window
     """
     def __init__(self, *args, **kw):
-        super(MainWindow, self).__init__(*args, **kw)
-        MainWindow.SizeScreen(self)
-        self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
-        self.Bind(wx.EVT_SIZE, self.OnResize)
+        super(Moria,self).__init__(*args, **kw)
+        self.initFrame()
+    
+    def initFrame(self):
+        #self.statusbar = self.CreateStatusBar()
+        #self.statusbar.SetStatusText("0")
+        #self.board = Board(self)
+        #self.board.SetFocus()
+
+        self.SetTitle("Moria")
+        self.Centre()  
+        self.panel=wx.Panel(self) 
+        self.panel.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.page0()
         
-    def SizeScreen(self):
+    def OnPaint(self, event):
+        image = wx.Bitmap("Images/MoriaGate.jpg")
+        image = wx.Bitmap.ConvertToImage(image)
         #Gets size of the screen width/height
         screensize = wx.DisplaySize()
         #If screen ration > 0.5625 limit width, < limit height
         if (screensize[0]/screensize[1])<= 0.5625:
-            self.SetSize(wx.Size(int((screensize[0]-100)*0.9), 
-                int((screensize[0]-100)/0.562*0.9)))
+            screensizing=(int((screensize[0]-100)*0.9), 
+                int((screensize[0]-100)/0.562*0.9))
         else:
-            self.SetSize(wx.Size(int((screensize[1]/16*9)*0.9), 
-                int(screensize[1]*0.9)))
+            screensizing=(int((screensize[1]/16*9)*0.9), 
+                int(screensize[1]*0.9))
+        width, height = screensizing
+        self.SetSize(screensizing)
+        image=image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
+        image = image.ConvertToBitmap()
+        dc=wx.PaintDC(self.panel)
+        dc.DrawBitmap(image, 0, 0)
         
-    def OnEraseBackground(self, evt):
-        """
-        Add a picture to the background
-        """
-        # yanked from ColourDB.py
-        dc = evt.GetDC()
-            
-        if not dc:
-            dc = wx.ClientDC(self)
-            rect = self.GetUpdateRegion().GetBox()
-            dc.SetClippingRect(rect)
-        dc.Clear()
-        bmp = wx.Bitmap("Images\MoriaGate.jpg")
-        dc.DrawBitmap(bmp, 0, 0)
-    
-    def OnResize(self, evt):
-        frame_size=self.GetSize()
-        image = wx.Image("Images\MoriaGate.jpg")
-        image.Rescale(frame_size[0],frame_size[1])
-        bmp = image.ConvertToBitmap()
-        self.DrawBitmap(bmp, 0, 0)
+    def page0(self):
+        wx.Button(self.panel, id=wx.ID_ANY, label="Order Tickets")
         
 if __name__=="__main__":
     #When the module is run, run the application.
     app = wx.App()
-    frm = MainWindow(None, title="MoriaTickets")
+    frm = Moria(None, title="MoriaTickets")
     frm.Show()
     app.MainLoop()
